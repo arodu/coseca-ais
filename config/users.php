acb17d6a7ecf5cedfa5504ab51d3bad9a3fcd791
Alberto Rodriguez <arodu.dev@gmail.com>
2022-10-28 13:36:08 -0400

    update config/users

--- config/users.php
+++ config/users.php
@@ -1,9 +1,20 @@
 <?php
 return [
-    'Users.Social.login' => true,
-    'OAuth.providers.facebook.options.clientId' => 'YOUR APP ID',
-    'OAuth.providers.facebook.options.clientSecret' => 'YOUR APP SECRET',
-    'OAuth.providers.twitter.options.clientId' => 'YOUR APP ID',
-    'OAuth.providers.twitter.options.clientSecret' => 'YOUR APP SECRET',
+    'Users' => [
+        'Social' => [
+            'login' => false,
+        ],
+        'Registration' => [
+            'defaultRole' => 'student',
+        ],
+        'Tos' => [
+            'required' => false,
+        ],
+    ],
+    
+    //'OAuth.providers.facebook.options.clientId' => 'YOUR APP ID',
+    //'OAuth.providers.facebook.options.clientSecret' => 'YOUR APP SECRET',
+    //'OAuth.providers.twitter.options.clientId' => 'YOUR APP ID',
+    //'OAuth.providers.twitter.options.clientSecret' => 'YOUR APP SECRET',
     //etc
 ];
