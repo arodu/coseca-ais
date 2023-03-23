22e48b09e5cfd5466c6f503db9a6e0ec3068c4e3
Alberto Rodriguez <arodu.dev@gmail.com>
2023-03-23 13:24:54 -0400

    feat: added current commit on menu

---
+++ templates/element/current_commit.php
@@ -0,0 +1,11 @@
+<?php
+
+$branch_name = shell_exec('git rev-parse --abbrev-ref HEAD');
+$last_commit_date = shell_exec('git log -1 --format=%ci');
+$last_commit_hash = shell_exec('git log -1 --format=%h');
+
+?>
+
+<div><?= __('branch: {0}', $branch_name) ?></div>
+<div><?= __('commit: {0}', $last_commit_hash) ?></div>
+<div><?= __('{0}', $last_commit_date) ?></div>
--- templates/plugin/CakeLte/element/sidebar/main.php
+++ templates/plugin/CakeLte/element/sidebar/main.php
@@ -7,4 +7,8 @@
     <ul class="nav nav-pills nav-sidebar flex-column <?= $this->CakeLte->getMenuClass() ?>" data-widget="treeview" role="menu" data-accordion="false">
         <?php echo $this->element('CakeLte.sidebar/menu') ?>
     </ul>
+
+    <div class="mt-5 text-center text-light">
+        <?= $this->element('current_commit') ?>
+    </div>
 </nav>
