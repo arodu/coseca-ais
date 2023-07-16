3657e6c52826c866f2cefa12283fbb5efdc00cb7
Alberto Rodriguez <arodu.dev@gmail.com>
2023-07-16 17:45:31 -0400

    fix: fix on format009

--- templates/Documents/format009.php
+++ templates/Documents/format009.php
@@ -5,6 +5,7 @@ $tutor = $student->principal_adscription->tutor;
 
 $this->assign('contentHeader', $this->element('Documents/format009Header'));
 $this->assign('contentFooter', $this->element('Documents/format009Footer'));
+
 ?>
 
 <h2>CONSTANCIA CULMINACIÓN SERVICIO COMUNITARIO</h2>
@@ -14,15 +15,16 @@ $this->assign('contentFooter', $this->element('Documents/format009Footer'));
     Bachiller: <strong><?= $student->full_name ?></strong>,
     titular de la Cédula de Identidad N° <strong><?= $student->dni ?></strong>,
     estudiante del <?= $student->tenant->program->getAreaPrintLabel() ?>,
-    CULMINÓ las actividades inherentes a la prestación del Servicio Comunitario
-    según lo establecido en la LEY DE SERVICIO COMUNITARIO DEL ESTUDIANTE DE EDUCACIÓN SUPERIOR y sus
-    REGLAMENTOS, a través de la ejecución del Proyecto: <strong><?= $project->name ?></strong>,
+    <strong>CULMINÓ</strong> las actividades inherentes a la prestación del Servicio Comunitario
+    según lo establecido en la <strong>LEY DE SERVICIO COMUNITARIO DEL ESTUDIANTE DE EDUCACIÓN SUPERIOR</strong> y sus
+    <strong>REGLAMENTOS</strong>, a través de la ejecución del Proyecto: <strong><?= $project->name ?></strong>,
     en la(s) Comunidad(es) o Institución: <strong><?= $project->institution->name ?></strong>,
     con el apoyo del(a) Tutor(a) Académico(a): <strong><?= $tutor->name ?></strong>.
 </p>
+
 <p>
-    Constancia que se expide a petición de la parte interesada en San Juan de
-    los Morros; a los <?= $endingStage->created->day ?> días del mes de <?= $this->App->month($endingStage->created->month) ?> de <?= $endingStage->created->year ?>.
+    Constancia que se expide a petición de la parte interesada en San Juan de los Morros;
+    a los <?= $endingStage->created->day ?> días del mes de <?= $this->App->month($endingStage->created->month) ?> de <?= $endingStage->created->year ?>.
 </p>
 
 <p>
