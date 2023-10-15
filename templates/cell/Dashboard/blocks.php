c9340aca47c33e6a89649765eeb4d1f7d3403dea
Alberto Rodriguez <arodu.dev@gmail.com>
2023-10-15 11:31:24 -0400

    minor fix

--- templates/cell/Dashboard/blocks.php
+++ templates/cell/Dashboard/blocks.php
@@ -11,18 +11,6 @@
 </div>
 <!-- /.col -->
 <div class="col-md-3 col-sm-6 col-12">
-    <div class="info-box bg-success">
-        <span class="info-box-icon"><i class="far fa-thumbs-up"></i></span>
-        <div class="info-box-content">
-            <span class="info-box-text"><?= __('Etapa Actual') ?></span>
-            <span class="info-box-number">N/A</span>
-        </div>
-        <!-- /.info-box-content -->
-    </div>
-    <!-- /.info-box -->
-</div>
-<!-- /.col -->
-<div class="col-md-3 col-sm-6 col-12">
     <div class="info-box bg-warning">
         <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
         <div class="info-box-content">
@@ -35,10 +23,22 @@
 </div>
 <!-- /.col -->
 <div class="col-md-3 col-sm-6 col-12">
-    <div class="info-box bg-dangesr">
+    <div class="info-box bg-danger">
         <span class="info-box-icon"><i class="fas fa-comments"></i></span>
         <div class="info-box-content">
             <span class="info-box-text"><?= __('Proyectos Activos') ?></span>
+            <span class="info-box-number">N/A</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
+<!-- /.col -->
+<div class="col-md-3 col-sm-6 col-12">
+    <div class="info-box bg-success">
+        <span class="info-box-icon"><i class="far fa-thumbs-up"></i></span>
+        <div class="info-box-content">
+            <span class="info-box-text"><?= __('Horas Completadas') ?></span>
             <span class="info-box-number">N/A</span>
         </div>
         <!-- /.info-box-content -->
