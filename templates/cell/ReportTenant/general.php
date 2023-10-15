6676d17390d5b2c2438d072d416580d5b3b1ce08
Alberto Rodriguez <arodu.dev@gmail.com>
2023-10-15 08:58:11 -0400

    refactor general tenant/report

--- templates/cell/ReportTenant/general.php
+++ templates/cell/ReportTenant/general.php
@@ -1,60 +1,45 @@
+<?php
+
+use App\Model\Field\StageField;
+use App\Model\Field\StageStatus;
+
+$statuses = [
+    StageStatus::WAITING,
+    StageStatus::IN_PROGRESS,
+    StageStatus::REVIEW,
+    StageStatus::SUCCESS,
+];
+
+$stages = [
+    StageField::REGISTER,
+    StageField::COURSE,
+    StageField::ADSCRIPTION,
+    StageField::TRACKING,
+    StageField::RESULTS,
+    StageField::ENDING,
+]
+?>
+
 <div class="row">
-    <div class="col-6">
-        <table class="table table-sm table-hover">
+    <div class="col-md-8">
+        <table class="table table-borderless table-hover">
+            <thead>
+                <tr>
+                    <th><?= __('Etapa / Estado') ?></th>
+                    <?php foreach ($statuses as $status) : ?>
+                        <th><?= $this->App->badge($status) ?></th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
             <tbody>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - registro: sin lapso') ?></th>
-                    <td><?= $studentWithoutLapse->count() ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - registro: en revision') ?></th>
-                    <td><?= $reports['register-review'] ?? 0 ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - registro: completado') ?></th>
-                    <td><?= $reports['register-success'] ?? 0 ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - taller: en espera') ?></th>
-                    <td><?= $reports['course-waiting'] ?? 0 ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - taller: completado') ?></th>
-                    <td><?= $reports['course-success'] ?? 0 ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - adscripcion: en espera') ?></th>
-                    <td><?= $reports['adscription-waiting'] ?? 0 ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('cantidad de estudiantes - adscripcion: completado') ?></th>
-                    <td><?= $reports['adscription-success'] ?? 0 ?></td>
-                </tr>
-
-                <tr>
-                    <th><?= __('cantidad de estudiantes - seguimiento: en proceso') ?></th>
-                    <td><?= $reports['tracking-in-progress'] ?? 0 ?></td>
-                </tr>
-
-                <tr>
-                    <th><?= __('cantidad de estudiantes - seguimiento: completado') ?></th>
-                    <td><?= $reports['tracking-success'] ?? 0 ?></td>
-                </tr>
-
-                <tr>
-                    <th><?= __('cantidad de estudiantes - resultados: completado') ?></th>
-                    <td><?= $reports['results-success'] ?? 0 ?></td>
-                </tr>
-
-                <tr>
-                    <th><?= __('cantidad de estudiantes - conclusion: en espera') ?></th>
-                    <td><?= $reports['ending-waiting'] ?? 0 ?></td>
-                </tr>
-
-                <tr>
-                    <th><?= __('cantidad de estudiantes - conclusion: completado') ?></th>
-                    <td><?= $reports['ending-success'] ?? 0 ?></td>
-                </tr>
+                <?php foreach ($stages as $stage) : ?>
+                    <tr>
+                        <th><?= $stage->label() ?></th>
+                        <?php foreach ($statuses as $status) : ?>
+                            <td><?= $reports[$stage->value . '-' . $status->value] ?? 0 ?></td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
     </div>
