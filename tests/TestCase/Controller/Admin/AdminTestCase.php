ce31d7604996b847974fabd864bb5f03f4458440
Alberto Rodriguez <arodu.dev@gmail.com>
2024-01-08 18:53:05 -0400

    test: add InstitutionsControllerTest, WIP

---
+++ tests/TestCase/Controller/Admin/AdminTestCase.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller\Admin;
+
+use App\Model\Entity\Student;
+use App\Model\Field\StudentType;
+use App\Model\Field\UserRole;
+use App\Test\Factory\CreateDataTrait;
+use App\Test\Factory\InstitutionFactory;
+use App\Test\Factory\TutorFactory;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+use Cake\Utility\Hash;
+
+abstract class AdminTestCase extends TestCase
+{
+    use IntegrationTestTrait;
+    use CreateDataTrait;
+
+    protected $program;
+    protected $tenant_id;
+    protected $user;
+    //protected $lapse_id;
+    //protected $tutors;
+    //protected $institution;
+    //protected $alertMessage = 'Comuniquese con la coordinación de servicio comunitario para mas información';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->enableRetainFlashMessages();
+        $this->enableCsrfToken();
+        $this->enableSecurityToken();
+
+        $this->program = $this->createProgram()->persist();
+        $this->user = $this->createUser(['role' => UserRole::ADMIN->value])->persist();
+        $this->tenant_id = Hash::get($this->program, 'tenants.0.id');
+        //$this->lapse_id = Hash::get($this->program, 'tenants.0.lapses.0.id');
+        //$this->setDefaultLapseDates($this->lapse_id);
+
+        //$this->tutors = TutorFactory::make([
+        //    'tenant_id' => $this->tenant_id,
+        //], 5)->persist();
+
+        //$this->institution = InstitutionFactory::make([
+        //    'tenant_id' => $this->tenant_id,
+        //])
+        //    ->with('InstitutionProjects', [], 5)
+        //    ->persist();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->program);
+        unset($this->user);
+        //unset($this->lapse_id);
+        unset($this->tenant_id);
+        //unset($this->tutors);
+        //unset($this->institution);
+
+        parent::tearDown();
+    }
+
+    protected function setAuthSession($user = null)
+    {
+        $user = $user ?? $this->user;
+
+        $this->session(['Auth' => $user]);
+    }
+}
---
+++ tests/TestCase/Controller/Admin/InstitutionsControllerTest.php
@@ -0,0 +1,153 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Test\TestCase\Controller\Admin;
+
+use App\Controller\Admin\InstitutionsController;
+use App\Model\Entity\Tenant;
+use App\Test\Factory\InstitutionFactory;
+use App\Test\Factory\ProgramFactory;
+use App\Test\Factory\TenantFactory;
+use App\Test\TestCase\Controller\Admin\AdminTestCase;
+use Cake\TestSuite\IntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * App\Controller\Admin\InstitutionsController Test Case
+ *
+ * @uses \App\Controller\Admin\InstitutionsController
+ */
+class InstitutionsControllerTest extends AdminTestCase
+{
+    use IntegrationTestTrait;
+
+    /**
+     * Test index method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::index()
+     */
+    public function testIndex(): void
+    {
+        $this->get('/admin/institutions');
+        $this->assertResponseCode(302);
+
+        $this->setAuthSession();
+        $this->get('/admin/institutions');
+        $this->assertResponseCode(200);
+
+        $intitution = InstitutionFactory::make([
+            'tenant_id' => $this->tenant_id,
+        ])->persist();
+
+        $this->get('/admin/institutions');
+        $this->assertResponseContains($intitution->name);
+        $this->assertResponseContains($intitution->contact_person);
+        $this->assertResponseContains($intitution->contact_phone);
+        $this->assertResponseContains($intitution->contact_email);
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::add()
+     */
+    public function testAddForm(): void
+    {
+        $this->setAuthSession();
+        
+        $this->get('/admin/institutions/add');
+        $this->assertResponseCode(200);
+    }
+
+    public function testAddPost(): void
+    {
+        $this->setAuthSession();
+
+        $this->post('/admin/institutions/add', [
+            'name' => 'Institución de prueba',
+            'contact_person' => 'Persona de prueba',
+            'contact_phone' => '04141234567',
+            'contact_email' => 'asd@asd.com',
+            'tenant_id' => $this->tenant_id,
+        ]);
+
+        $this->assertResponseContains('Institución de prueba');
+        $this->assertResponseContains('Persona de prueba');
+        $this->assertResponseContains('04141234567');
+        $this->assertResponseContains('asd@asd.com');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::view()
+     */
+    //public function testView(): void
+    //{
+    //    $this->markTestIncomplete('Not implemented yet.');
+    //}
+
+    /**
+     * Test addProject method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::addProject()
+     */
+    //public function testAddProject(): void
+    //{
+    //    $this->markTestIncomplete('Not implemented yet.');
+    //}
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::edit()
+     */
+    public function testEdit(): void
+    {
+        $this->setAuthSession();
+
+        $intitution = InstitutionFactory::make([
+            'tenant_id' => $this->tenant_id,
+        ])->persist();
+        
+        $this->get('/admin/institutions/edit/' . $intitution->id);
+        $this->assertResponseCode(200);
+
+        $this->post('/admin/institutions/edit/' . $intitution->id, [
+            'name' => 'prueba edit',
+        ]);
+        $this->assertResponseCode(302);
+
+        $this->get('/admin/institutions/view/' . $intitution->id);
+        $this->assertResponseContains('prueba edit');
+        $this->assertResponseNotContains($intitution->name);
+    }
+
+    /**
+     * Test editProject method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::editProject()
+     */
+    //public function testEditProject(): void
+    //{
+    //    $this->markTestIncomplete('Not implemented yet.');
+    //}
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     * @uses \App\Controller\Admin\InstitutionsController::delete()
+     */
+    //public function testDelete(): void
+    //{
+    //    $this->markTestIncomplete('Not implemented yet.');
+    //}
+}
