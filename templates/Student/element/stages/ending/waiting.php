e9e255f527b0ac076263f231a6d5f0f3c1410f3c
Alberto Rodriguez <arodu.dev@gmail.com>
2024-02-11 11:50:22 -0400

    fix: update format009 button

--- templates/Student/element/stages/ending/waiting.php
+++ templates/Student/element/stages/ending/waiting.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * @var \App\View\AppView $this
  * @var \App\Model\Entity\Student $student
  * @var \App\Model\Entity\StudentStage $studentStage
  */
@@ -15,14 +16,14 @@ use App\Enum\ActionColor;
 <?php else : ?>
     <p><?= __('Estimado Prestador de Servicio Comunitario, estamos complacidos de haberte acompañado hasta el final de tu proceso en el lapso académico 2023-1.  Para acceder a tu planilla 009, te invitamos a imprimirla, y llevarla a nuestra oficina AIS para validarla. Una vez firmada, podrás retirarla para tramitar los procesos inherentes.') ?></p>
 
-    <?= $this->Button->report([
+    <?= $this->Button->fileReport([
         'url' => [
             'prefix' => 'Student',
             'controller' => 'Documents',
             'action' => 'format009',
         ],
         'label' => __('Descargar planilla 009'),
-        'actionColor' => ActionColor::EDIT,
         'target' => '_blank',
+        'class' => 'btn-sm'
     ]) ?>
 <?php endif ?>
