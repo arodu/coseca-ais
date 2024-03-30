36711d7df4225a1e5e77436306afa5abfc3fa660
Alberto Rodriguez <arodu.dev@gmail.com>
2024-03-30 12:09:49 -0400

    Refactor user view template

--- plugins/Manager/templates/Users/view.php
+++ plugins/Manager/templates/Users/view.php
@@ -3,7 +3,11 @@
 /**
  * @var \App\View\AppView $this
  * @var \Cake\Datasource\EntityInterface $user
+ * @var \App\Model\Entity\AppUser $user
  */
+
+use App\Model\Field\UserRole;
+
 ?>
 
 <?php
@@ -17,106 +21,82 @@ $this->Breadcrumbs->add([
 ?>
 
 <div class="view card card-primary card-outline">
-  <div class="card-header d-sm-flex">
-    <h2 class="card-title"><?= h($user->username) ?></h2>
-  </div>
-  <div class="card-body table-responsive p-0">
-    <table class="table table-hover text-nowrap">
-        <tr>
-            <th><?= __('Id') ?></th>
-            <td><?= h($user->id) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Username') ?></th>
-            <td><?= h($user->username) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Email') ?></th>
-            <td><?= h($user->email) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Dni') ?></th>
-            <td><?= h($user->dni) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('First Name') ?></th>
-            <td><?= h($user->first_name) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Last Name') ?></th>
-            <td><?= h($user->last_name) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Api Token') ?></th>
-            <td><?= h($user->api_token) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Secret') ?></th>
-            <td><?= h($user->secret) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Role') ?></th>
-            <td><?= h($user->role) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Token Expires') ?></th>
-            <td><?= h($user->token_expires) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Activation Date') ?></th>
-            <td><?= h($user->activation_date) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Tos Date') ?></th>
-            <td><?= h($user->tos_date) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Created') ?></th>
-            <td><?= h($user->created) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Modified') ?></th>
-            <td><?= h($user->modified) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Last Login') ?></th>
-            <td><?= h($user->last_login) ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Secret Verified') ?></th>
-            <td><?= $user->secret_verified ? __('Yes') : __('No'); ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Active') ?></th>
-            <td><?= $user->active ? __('Yes') : __('No'); ?></td>
-        </tr>
-        <tr>
-            <th><?= __('Is Superuser') ?></th>
-            <td><?= $user->is_superuser ? __('Yes') : __('No'); ?></td>
-        </tr>
-    </table>
-  </div>
-  <div class="card-footer d-flex">
-    <div class="">
-      <?= $this->Form->postLink(
-          __('Delete'),
-          ['action' => 'delete', $user->id],
-          ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'btn btn-danger']
-      ) ?>
+    <div class="card-header d-sm-flex">
+        <h2 class="card-title"><?= h($user->full_name) ?></h2>
     </div>
-    <div class="ml-auto">
-      <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id], ['class' => 'btn btn-secondary']) ?>
-      <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
+    <div class="card-body table-responsive p-0">
+        <table class="table table-hover text-nowrap">
+            <tr>
+                <th><?= __('Email') ?></th>
+                <td><?= h($user->email) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Dni') ?></th>
+                <td><?= h($user->dni) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('First Name') ?></th>
+                <td><?= h($user->first_name) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Last Name') ?></th>
+                <td><?= h($user->last_name) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Role') ?></th>
+                <td><?= h($user->enum('role')->label()) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Created') ?></th>
+                <td><?= h($user->created) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Modified') ?></th>
+                <td><?= h($user->modified) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Last Login') ?></th>
+                <td><?= h($user->last_login) ?></td>
+            </tr>
+            <tr>
+                <th><?= __('Active') ?></th>
+                <td><?= $user->active ? __('Yes') : __('No'); ?></td>
+            </tr>
+        </table>
     </div>
-  </div>
+    <div class="card-footer d-flex">
+        <div class="">
+            <?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $user->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'btn btn-danger']
+            ) ?>
+        </div>
+        <div class="ml-auto">
+            <?= $this->Html->link(__('Edit'), ['action' => 'edit', $user->id], ['class' => 'btn btn-secondary']) ?>
+            <?= $this->Html->link(__('Cancel'), ['action' => 'index'], ['class' => 'btn btn-default']) ?>
+        </div>
+    </div>
 </div>
 
-<div class="view text card">
-  <div class="card-header">
-    <h3 class="card-title"><?= __('Additional Data') ?></h3>
-  </div>
-  <div class="card-body">
-    <?= $this->Text->autoParagraph(h($user->additional_data)); ?>
-  </div>
-</div>
+<?php if ($user->enum('role')->isGroup(UserRole::GROUP_STUDENT)) : ?>
+    <div class="card card-primary card-outline">
+        <div class="card-header">
+            <h3 class="card-title">Estudiante</h3>
+        </div>
+        <div class="card-body">
+            <p>Estudiante</p>
+        </div>
+    </div>
+<?php endif; ?>
 
+<?php if ($user->enum('role')->isGroup(UserRole::GROUP_STAFF)) : ?>
+    <div class="card card-primary card-outline">
+        <div class="card-header">
+            <h3 class="card-title">Staff</h3>
+        </div>
+        <div class="card-body">
+            <p>Staff</p>
+        </div>
+    </div>
+<?php endif; ?>
