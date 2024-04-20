c65827d7e1e1a5ebb945ef43da396d6d2120e7c3
Anthony Carrillo <anthonyzok521@gmail.com>
2024-04-19 21:27:01 -0400

    fixed: Fixed Bug in page 404 Error. New template of error

--- templates/Error/error400.php
+++ templates/Error/error400.php
@@ -34,8 +34,32 @@ if (Configure::read('debug')) :
 $this->end();
 endif;
 ?>
-<h2><?= h($message) ?></h2>
-<p class="error">
-    <strong><?= __d('cake', 'Error') ?>: </strong>
-    <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
-</p>
+
+<?php 
+//Format $message to return back or logout
+
+    $back = 'javascript:history.back()'; //to return back
+
+    if(strstr($message, '/logout')){            //exist?
+        $back = explode(". ", $message)[1];     //to logout
+        $message = explode(". ", $message)[0];  //The message of error
+    }
+    
+
+?>
+
+<img class='img-404' src="https://img.freepik.com/vector-gratis/concepto-fallo-tecnico-landing-page_52683-12188.jpg" alt="ERRO 404">
+<div class='code'>
+    <div class='title-error'>
+        <i class="fi fi-sr-triangle-warning"></i>
+        <h2>Oops! Pagina no encontrada.</h2>
+    </div>
+</div>
+<hr>
+<div class="content">
+    <h2><?= h($message) ?>. <?= $this->Html->link(__('Volver'), $back) ?></h2>
+    <p class="error">
+        <strong><?= __d('cake', 'Error') ?>: </strong>
+        <?= __d('cake', 'The requested address {0} was not found on this server.', "<strong>'{$url}'</strong>") ?>
+    </p>
+</div>
--- templates/layout/error.php
+++ templates/layout/error.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * Copyright 2024 ACGAMES - anthonyzok521@gmail.com
+ * 
+ * Template for 404 ERROR!
+ * 
  * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
@@ -17,6 +21,10 @@
 <!DOCTYPE html>
 <html>
 <head>
+
+    <?php  // Responsive ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <?= $this->Html->charset() ?>
     <title>
         <?= $this->fetch('title') ?>
@@ -24,18 +32,128 @@
     <?= $this->Html->meta('icon') ?>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-
+    <?php  // Icon Warning since flaticon.com ?>
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.3.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
+
+    <?php  // Style of template 404 error ?>
+    <style>
+        :root{
+            --color-warning: #ffc107;
+            --bg: #fafafa;
+        }
+
+        body{
+            background-color: var(--bg);
+            display: flex;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        hr{
+            width: 100%;
+            border: solid 6px;
+            margin: 0;
+        }
+
+        .img-404{
+            clip-path: inset(23px 17px);
+            margin-top: 68px;
+        }
+
+        .error-container{
+            width: 80%;
+            height: 500px;
+        }
+
+        .code {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .code h2 {
+            width: max-content;
+            font-weight: 800;
+            margin: 0;
+            font-size: 1.6rem;
+        }
+
+        .title-error{
+            display: flex;
+            align-items: center;
+            height: max-content;
+        }
+
+        .fi-sr-triangle-warning{
+            margin-top: 5px;
+            margin-right: 5px;
+            color: var(--color-warning);
+            font-size: 16px;
+        }
+
+        .content h2 {
+            font-size: 2.2rem;          
+        }
+
+        .error{
+            font-size: 10px;
+        }
+
+        @media screen and (width > 600px) {
+            .content h2 {
+                font-size: 20px;          
+            }
+        
+            .img-404{
+                object-fit: fill;
+                height: 315px;
+                margin-top: 20px;
+                width: 65%;
+            }
+
+            .code {
+                height: none;
+            }
+
+            .error-container{
+                height: 100vh;
+                width: 80%;
+                display: flex;
+                justify-content: flex-start;
+            }
+
+            .fi-sr-triangle-warning{
+                margin-top: 4;
+                font-size: 21px;
+            }
+
+            .code h2 {
+                margin: 0;
+                font-size: 18px;
+            }
+
+            .content{
+                width: 607px;
+            }
+
+            hr{
+                margin-top: 50px;
+                width: 50%;
+            }
+        }
+    </style>
 </head>
 <body>
     <div class="error-container">
         <?= $this->Flash->render() ?>
         <?= $this->fetch('content') ?>
-        <?= $this->Html->link(__('Back'), 'javascript:history.back()') ?>
+        <?php  // Back button removed. Moved to template/Error/error400.php ?>
     </div>
 </body>
 </html>
