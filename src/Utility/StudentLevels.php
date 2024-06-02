564b59b6edf18ebedcc053a6a1e90e241a894216
Smilee21 <antomigel23@gmail.com>
2024-06-02 15:25:31 -0400

    methods were added to obtain the type of regimen and whether it is UC or not and the getArray method was modified

--- src/Utility/StudentLevels.php
+++ src/Utility/StudentLevels.php
@@ -28,13 +28,34 @@ class StudentLevels
         return ProgramRegime::from($program->regime)->formLabel();
     }
 
+
+    public static function getYearOrSemestre(Program $program): array
+    {
+
+       return ProgramRegime::from($program->regime)->creditTypeLabel();
+        
+    }
+
+    public static function getMinAndMax(Program $program): array
+    {
+
+       return ProgramRegime::from($program->regime)->minAndMax();
+        
+    }
+
+    
+
     /**
      * @param int $maxLevel
      * @return array
      */
     protected static function getArray(int $maxLevel): array
     {
-        $min = floor($maxLevel / 2) + 1;
+     
+        $min = floor($maxLevel / 2) +1;     
+        
+        if ($min <= 3) $min = $min + 1;
+    
         $range = range($min, $maxLevel);
         $cardinals = array_map(fn ($level) => $level . '°', $range);
 
