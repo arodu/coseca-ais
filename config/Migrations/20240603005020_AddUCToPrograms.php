4afbcdfdfa018bd62ed7d528901c8a468d0ac0f0
Smilee21 <antomigel23@gmail.com>
2024-06-03 19:09:09 -0400

    add field to save credit units in programs

---
+++ config/Migrations/20240603005020_AddUCToPrograms.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddUCToPrograms extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('programs');
+        $table->addColumn('uc', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
