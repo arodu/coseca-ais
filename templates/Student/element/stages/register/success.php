170f5942aeabbf1e43a2b6f120128f2aa21419d6
Alberto Rodriguez <arodu.dev@gmail.com>
2025-12-24 17:24:22 -0400

    fix: escape output in success.php to prevent XSS vulnerabilities

--- templates/Student/element/stages/register/success.php
+++ templates/Student/element/stages/register/success.php
@@ -6,26 +6,26 @@
 ?>
 <dl class="row">
     <dt class="col-sm-3 text-right"><?= __('Cédula') ?></dt>
-    <dd class="col-sm-7"><?= $student->dni ?> </dd>
+    <dd class="col-sm-7"><?= h($student->dni) ?> </dd>
     
     <dt class="col-sm-3 text-right"><?= __('Nombres') ?></dt>
-    <dd class="col-sm-7"><?= $student->first_name ?> </dd>
+    <dd class="col-sm-7"><?= h($student->first_name) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Apellidos') ?></dt>
-    <dd class="col-sm-7"><?= $student->last_name ?> </dd>
+    <dd class="col-sm-7"><?= h($student->last_name) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Correo electrónico') ?></dt>
-    <dd class="col-sm-7"><?= $student->email ?> </dd>
+    <dd class="col-sm-7"><?= h($student->email) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Programa') ?></dt>
-    <dd class="col-sm-7"><?= $student->tenant->label ?> </dd>
+    <dd class="col-sm-7"><?= h($student->tenant->label) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Teléfono') ?></dt>
-    <dd class="col-sm-7"><?= $student->student_data->phone ?> </dd>
+    <dd class="col-sm-7"><?= h($student->student_data?->phone) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Área de Interés') ?></dt>
-    <dd class="col-sm-7"><?= $student->student_data->interest_area->name ?> </dd>
+    <dd class="col-sm-7"><?= h($student->student_data?->interest_area?->name) ?> </dd>
 
     <dt class="col-sm-3 text-right"><?= __('Registro') ?></dt>
-    <dd class="col-sm-7"><?= $student->created ?> </dd>
+    <dd class="col-sm-7"><?= h($student->created) ?> </dd>
 </dl>
